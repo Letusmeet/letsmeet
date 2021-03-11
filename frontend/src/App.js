5126493d013cae7e15fbfa9f06c3f9d925c93f37
amar-rokade <rokade6117@gmail.com>
2021-03-12 02:01:21 +0530

    auth added login and signup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./base/Footer";
+import Navbar from "./base/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Login from "./accounts/Login";
+import Signup from "./accounts/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+      <h1>Hii</h1>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/base/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <h1>Footer</h1>
+        <ul>
+          <li>vjnfv</li>
+          <li>vjnfv</li>
+          <li>vjnfv</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
