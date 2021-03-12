0d8f56cfceb8cae6158f1e2a9c21725ccc1217b3
Rohit-sharma14 <14rohit09sharma99@gmail.com>
2021-03-12 21:54:03 +0530

    room.js

---
+++ backend/models/room.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const RoomSchema = new mongoose.Schema({
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    members: [{
+        memberid: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        designation: {
+            type: String,
+            required: true,
+            default: "Employee"
+        }
+    }],
+    boards: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Board'
+    }],
+    Roomicon: {
+        type: String
+    },
+    conversation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'conversations'
+    }
+
+})
+
+module.exports = mongoose.model('Room', RoomSchema)
