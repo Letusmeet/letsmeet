3cf4be6df0e7ea18c9dfb19e182a01417499e17b
amar-rokade <rokade6117@gmail.com>
2021-03-13 14:43:43 +0530

    add

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
->>>>>>> d56141d5f7b802c136640e5fe45b86278e756805
