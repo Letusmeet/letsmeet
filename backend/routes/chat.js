ddddd9eb00713f23ffb072af4b48d23aa3a6e7a8
Rohit-Sharma14 <14rohit09sharma99@gmail.com>
2021-03-13 14:10:18 +0530

    error resolved

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const middleware = require('../middleware/user')
+const middlewareadmin = require('../middleware/admin')
 const Message = require("../models/Message");
 const Conversation = require("../models/Conversation");
 
