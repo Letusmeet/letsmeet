ae6db0c93b47947f4e507264d5d5181797ae5ba8
Nishi Agrawal <nishihere19@gmail.com>
2021-03-13 21:48:14 +0530

    minor bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import reactDom from "react-dom";
-import "./App.css";
-import Footer from "./base/Footer";
-import { Switch, Route } from "react-router-dom";
-import Login from "./accounts/Login";
-import Signup from "./accounts/Signup";
-import UserAuth from "./accounts/UserAuth";
-import React from "react";
-import AuthNav from "./base/AuthNav";
-import UnAuthNav from "./base/UnAuthNav";
-import Chat from "./Chat/Chat"
-
-function App() {
-  const [authenticated, setAuthenticated] = React.useState(true);
-  return (
-    <>
-      <UserAuth.Provider value={{ authenticated, setAuthenticated }}>
-     {authenticated ? <AuthNav /> : <UnAuthNav />}
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/chat" component={Chat} />
-        </Switch>
-        <Footer />
-      </UserAuth.Provider>
-    </>
-  );
-}
-
-export default App;
-=======
 import reactDom from "react-dom";
 import "./App.css";
 import Footer from "./base/Footer";
@@ -67,4 +35,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 51b0a765fe0bb781112e1bd596d0f75a460c5a8b
