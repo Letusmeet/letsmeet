f413f9e7393ae6e80bf35f6761447f95d446da55
Rohit-Sharma14 <14rohit09sharma99@gmail.com>
2021-03-13 22:21:59 +0530

    added fetch room route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,26 +11,26 @@ const middleware = require('../middleware/user')
 
 
 //to send request to any user
-router.post('/sendrequest/:id',middleware,(req,res)=>{
-    User.findByIdAndUpdate(req.params.id,{$addToSet:{requests:req.user._id}}).then(friend=>{
-        req.user.updateOne({$addToSet:{sent:req.params.id}}).then(sent=>{
-            console.log(friend,sent);
+router.post('/sendrequest/:id', middleware, (req, res) => {
+    User.findByIdAndUpdate(req.params.id, { $addToSet: { requests: req.user._id } }).then(friend => {
+        req.user.updateOne({ $addToSet: { sent: req.params.id } }).then(sent => {
+            console.log(friend, sent);
             res.status(200).json("Request sent succesfully");
         })
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
         res.status(500).json("Some error occured");
     })
 })
 
 //to accept a friend request
-router.post('/acceptrequest/:id',middleware,(req,res)=>{
-    User.findByIdAndUpdate(req.params.id,{$pull:{sent:req.user._id}}).then(friend=>{
-        req.user.updateOne({$addToSet:{friends:req.params.id},$pull:{requests:req.params.id}}).then(added=>{
-            console.log(friend,added);
+router.post('/acceptrequest/:id', middleware, (req, res) => {
+    User.findByIdAndUpdate(req.params.id, { $pull: { sent: req.user._id } }).then(friend => {
+        req.user.updateOne({ $addToSet: { friends: req.params.id }, $pull: { requests: req.params.id } }).then(added => {
+            console.log(friend, added);
             res.status(200).json("Request accepted succesfully");
         })
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
         res.status(500).json("Some error occured");
     })
@@ -38,25 +38,31 @@ router.post('/acceptrequest/:id',middleware,(req,res)=>{
 
 
 //to deny a friend request
-router.post('/denyrequest/:id',middleware,(req,res)=>{
-    User.findByIdAndUpdate(req.params.id,{$pull:{sent:req.user._id}}).then(friend=>{
-        req.user.updateOne({$pull:{requests:req.params.id}}).then(denied=>{
-            console.log(friend,denied);
+router.post('/denyrequest/:id', middleware, (req, res) => {
+    User.findByIdAndUpdate(req.params.id, { $pull: { sent: req.user._id } }).then(friend => {
+        req.user.updateOne({ $pull: { requests: req.params.id } }).then(denied => {
+            console.log(friend, denied);
             res.status(200).json("Request rejected succesfully");
         })
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
         res.status(500).json("Some error occured");
     })
 })
 
 //to fetch all requests
-router.post('/myrequests',middleware,(req,res)=>{
-    User.findById(req.user._id).then(me=>{
+router.post('/myrequests', middleware, (req, res) => {
+    User.findById(req.user._id).then(me => {
         res.status(200).json(me.requests);
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json("Some error occured, please try again!");
     })
+})
+
+
+//to fetch the rooms he is in
+router.post('/fetchroom', middleware, (req, res) => {
+    res.json(req.user.rooms);
 })
 
 module.exports = router;
